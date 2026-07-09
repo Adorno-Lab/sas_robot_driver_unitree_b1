ee1d2d4dcd99689c664b42996d783ee72a6c08e0
Juancho <juanjqogm@gmail.com>
2026-07-09 16:33:17 +0100

    Added a new class for free flying robot drivers

---
+++ src/FreeFlyingRobotDriver.cpp
@@ -0,0 +1,15 @@
+#include "FreeFlyingRobotDriver.hpp"
+
+namespace sas
+{
+
+FreeFlyingRobotDriver::FreeFlyingRobotDriver(std::shared_ptr<Node>& node,
+                                             std::atomic_bool* break_loops)
+:RobotDriver{break_loops}
+{
+
+
+}
+
+
+}
